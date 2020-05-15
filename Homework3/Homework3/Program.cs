998c03d18d68d91356c79010a62da2c617e7615e
AytakinAghayeva1176 <aytakinia@code.edu.az>
2020-05-15 23:41:13 +0400

    done

---
+++ Homework3/Homework3/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Achar sozu daxil edin:");
+            string key = Console.ReadLine();
+
+            DocumentProgram docProgram;
+
+            switch (key)
+            {
+                case "basic":
+                    docProgram = new DocumentProgram();
+                    break;
+                case "pro":
+                    docProgram = new ProDocumentProgram();
+                    break;
+                case "expert ":
+                    docProgram = new ExpertDocument();
+                    break;
+
+                default:
+                    return;
+            }
+
+            ShowMethods(docProgram);
+        }
+
+        static void ShowMethods(DocumentProgram docProgram)
+        {
+            docProgram.OpenDocument();
+            docProgram.EditDocument();
+            docProgram.SaveDocument();
+        }
+    }
+
+
+    class DocumentProgram
+    {
+
+        public void OpenDocument()
+        {
+            Console.WriteLine("Document Opened");
+        }
+
+        public virtual void EditDocument()
+        {
+            Console.WriteLine("Can Edit in Pro and Expert versions");
+
+        }
+
+        public virtual void SaveDocument()
+        {
+            Console.WriteLine("Can Save in Pro and Expert versions");
+
+        }
+    }
+
+
+    class ProDocumentProgram : DocumentProgram
+    {
+        public sealed override void EditDocument()
+        {
+            Console.WriteLine("Document Edited");
+        }
+
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Document Saved in doc format, for pdf format buy Expert packet");
+
+        }
+
+
+    }
+
+    class ExpertDocument : ProDocumentProgram
+    {
+
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Document Saved in pdf format");
+
+        }
+
+    }
+}
